589e9d5f20b12918adcded2444412c59c35f7326
polakovap <s00239846@atu.ie>
2025-02-17 15:40:27 +0000

    screenshot of test results added

--- src/app/bascomputics/compute.spec.ts
+++ src/app/bascomputics/compute.spec.ts
@@ -1,7 +1,7 @@
 import { compute } from "./compute";
 
 //describe explains that the test is compute
-describe ('compute', () => {
+describe ('Compute', () => {
     //it explains that the returned value should be 0
     it('should return 0 if the input is negative', () => {
         //compute function is called and passed -1
@@ -11,7 +11,7 @@ describe ('compute', () => {
     });
 
     //it explains that the returned value should be 0
-    it ("should return input+1 if the input is positive", () => {
+    it ('should return input+1 if the input is positive', () => {
         //compute function is called and passed 1
         const result = compute(1);
         //expect checks if 2 was returned
--- src/app/strings/greet.spec.ts
+++ src/app/strings/greet.spec.ts
@@ -1,6 +1,6 @@
 import { greet } from "./greet";
 
-describe('greet', () => {
+describe('Greet - Petra Poláková, S00239846', () => {
     //check that return from greet function contains the name
     it('it should include the name in the message', () => {
         expect(greet('Jeff')).toContain('Jeff');
